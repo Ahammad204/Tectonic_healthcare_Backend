499995fdf4ab6a26aee0a839561535aa3e7ca532
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-08-12 22:10:39 +0600

    refactor: streamline API entry point and remove unused vercel configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/server.ts", "src/vercel.ts"],
+  entry: ["src/server.ts"],
   format: ["esm"],
   target: "esnext",
   outDir: "dist",
